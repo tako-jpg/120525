9bebd85856610c9ef7798e2300c5a2bd073f51a7
pt-kt <tany162@mail.ru>
2025-05-19 11:55:54 +0300

    сдаю ДЗ Массивы 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
+
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Задача 1");
+        int [] numbers = new int [] {1, 2 , 3};
+        double [] fractionalNumber = {1.57, 7.654, 9.986};
+        int [] numbersArbitrary = new int [] {5, 3, 8};
+
+
+        System.out.println("Задача 2");
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(fractionalNumber));
+        System.out.println(Arrays.toString(numbersArbitrary));
+
+        System.out.println("Задача 3");
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            if (i > 0) {
+                System.out.print(numbers[i] + ", ");
+            } else {
+                System.out.print(numbers[i]);
+            }
+        }
+        System.out.println();
+        for (int i = fractionalNumber.length - 1; i >= 0; i--) {
+            if (i > 0) {
+                System.out.print(fractionalNumber[i] + ", ");
+            } else {
+                System.out.print(fractionalNumber[i]);
+            }
+        }
+        System.out.println();
+        for (int i = numbersArbitrary.length - 1; i >= 0; i--) {
+            if (i > 0) {
+                System.out.print(numbersArbitrary[i] + ", ");
+            } else {
+                System.out.print(numbersArbitrary[i]);
+            }
+        }
+        System.out.println();
+        System.out.println("Задача 4");
+        for (int i = 0; i < numbers.length; i ++) {
+            if (numbers [i] % 2 == 1) {
+                numbers[i] = numbers[i] + 1;
+            }
+        }
+            {
+            System.out.print(Arrays.toString(numbers));
+            }
     }
 }
+
+
+
+
